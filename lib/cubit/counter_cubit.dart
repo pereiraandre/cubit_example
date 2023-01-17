dbcaab7870bc3dcac360cea9740f2ff66d36002c
Norberto <n.g.vasconcelos@gmail.com>
2023-01-17 15:09:15 +0000

    Reduced states & removed state variable from cubit

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -1,27 +1,15 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
 
 part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
-  CounterCubit() : super(CounterInitial());
+  CounterCubit() : super(CounterLoaded(number: 0));
 
-  int number = 1;
-  void incrementNumber(){
-    emit(CounterLoading());
-    try{
-      emit(CounterLoaded(number: number++));
-    }catch(e){
-      emit(CounterError(errorMessage: e.toString()));
-    }
+  void incrementNumber() {
+    emit(CounterLoaded(number: (state as CounterLoaded).number + 1));
   }
 
-  void decrementNumber(){
-    emit(CounterLoading());
-    try{
-      emit(CounterLoaded(number: number--));
-    }catch(e){
-      emit(CounterError(errorMessage: e.toString()));
-    }
+  void decrementNumber() {
+    emit(CounterLoaded(number: (state as CounterLoaded).number - 1));
   }
 }
--- lib/cubit/counter_state.dart
+++ lib/cubit/counter_state.dart
@@ -1,20 +1,9 @@
 part of 'counter_cubit.dart';
 
-@immutable
 abstract class CounterState {}
-
-class CounterInitial extends CounterState {}
-
-class CounterLoading extends CounterState {}
 
 class CounterLoaded extends CounterState {
   final int number;
 
   CounterLoaded({required this.number});
-}
-
-class CounterError extends CounterState {
-  final String errorMessage;
-
-  CounterError({required this.errorMessage});
 }
